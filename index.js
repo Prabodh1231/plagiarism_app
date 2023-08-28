7d43b9071a6827e73911482b5640bde6169909ba
prabodh_pop! <prabodh1231@outlook.com>
2023-08-29 00:37:43 +0530

    made change in loop

--- index.js
+++ index.js
@@ -4,38 +4,26 @@ function compareText() {
 
     let arrayClientInput = clientInput.split(" ");
     let arrayCompareInput = compareInput.split(" ");
-    let sizeOfcompairArray = arrayCompareInput.length - 13;
-    let sizeOfClientInput = arrayClientInput.length - 13;
+    let sizeOfcompairArray = arrayCompareInput.length;
+    let sizeOfClientInput = arrayClientInput.length;
 
     function findCommonElements(array1, array2) {
         let copyClientInput01 = arrayClientInput; // Create a copy
         let copyClientInput02 = arrayClientInput; // Create another copy
-        let startOfCompare = 0;
-        let endOfCompare = 13;
-
         for (let i = 0; i < sizeOfcompairArray; i++) {
+            let startOfCompare = i;
+            let endOfCompare = i + 13;
             let compareOther = arrayCompareInput.slice(startOfCompare, endOfCompare);
-            let startOfClient = 0;
-            let endOfClient = 13;
             for (let index = 0; index < sizeOfClientInput; index++) {
+                let startOfClient = index;
+                let endOfClient = index + 13;
                 let compareClient = copyClientInput02.slice(startOfClient, endOfClient);
+                console.log(compareOther)
                 if (compareClient.every(compare => compareOther.includes(compare))) {
                     let colorCommonElement = compareClient.map((common) => `<span style="color:red"> ${common}</span>`)
                     copyClientInput01.splice(startOfClient, 13, ...colorCommonElement);
-                    startOfClient += 13;
-                    endOfClient += 13;
-                    //console.log(startOfClient)
-                } 
-                else {
-                    startOfClient ++;
-                 //   console.log(startOfClient)
-                    endOfClient ++;
-                }
-                
+                }               
             }
-            startOfCompare++;
-            console.log(startOfCompare)
-            endOfCompare++;
         }
         return copyClientInput01;
     }
