5050617ff0c686f57f5bc01c0325631c8de2cd6b
prabodh_pop! <prabodh1231@outlook.com>
2023-08-28 22:23:26 +0530

    Compare a block of 14

--- index.js
+++ index.js
@@ -4,21 +4,42 @@ function compareText() {
 
     let arrayClientInput = clientInput.split(" ");
     let arrayCompareInput = compareInput.split(" ");
+    let sizeOfcompairArray = arrayCompareInput.length - 13;
+    let sizeOfClientInput = arrayClientInput.length - 13;
 
     function findCommonElements(array1, array2) {
-        let commonElements = [];
+        let copyClientInput01 = arrayClientInput; // Create a copy
+        let copyClientInput02 = arrayClientInput; // Create another copy
+        let startOfCompare = 0;
+        let endOfCompare = 13;
 
-        for (let i = 0; i < array1.length; i++) {
-            if (array2.includes(array1[i])) {
-                commonElements.push(`<span style="color: red">${array1[i]}</span>`);
-            } else {
-                commonElements.push(array1[i]);
+        for (let i = 0; i < sizeOfcompairArray; i++) {
+            let compareOther = arrayCompareInput.slice(startOfCompare, endOfCompare);
+            let startOfClient = 0;
+            let endOfClient = 13;
+            for (let index = 0; index < sizeOfClientInput; index++) {
+                let compareClient = copyClientInput02.slice(startOfClient, endOfClient);
+                if (compareClient.every(compare => compareOther.includes(compare))) {
+                    let colorCommonElement = compareClient.map((common) => `<span style="color:red"> ${common}</span>`)
+                    copyClientInput01.splice(startOfClient, 13, ...colorCommonElement);
+                    startOfClient += 13;
+                    endOfClient += 13;
+                    //console.log(startOfClient)
+                } 
+                else {
+                    startOfClient ++;
+                 //   console.log(startOfClient)
+                    endOfClient ++;
+                }
+                
             }
+            startOfCompare++;
+            console.log(startOfCompare)
+            endOfCompare++;
         }
-
-        return commonElements;
+        return copyClientInput01;
     }
-
+    
     let highlightedText = findCommonElements(arrayClientInput, arrayCompareInput);
 
     // Join the elements to create a formatted string
